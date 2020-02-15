03399f1a8849e4330412f83a175db31111dfe8bc
jtwray <tuckerwray84@gmail.com>
2020-02-15 21:32:24 +0000

    dependencies :

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+const compression =require('compression');
 const express = require('express');
 const app = express();
-const port = 3000;
+app.use(compression());
+
+const port = 3000 || process.env.PORT;
 
 app.get(`/`, (req,res) =>{
 	res.send('Hello WizOrld');
+
 });
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}`));
