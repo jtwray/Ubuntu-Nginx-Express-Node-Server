0d253da533ba95eafc5f00ba5cc84a49e97c4b8d
jtwray <tuckerwray84@gmail.com>
2020-02-17 01:23:36 -0500

    nginx handling gzip

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const compression =require('compression');
+
 const express = require('express');
 const app = express();
-app.use(compression());
+
 
 const port = 3000 || process.env.PORT;
 
 app.get(`/`, (req,res) =>{
-	res.send('Hello WizOrld');
+	res.send('Hello World dependencies updated');
 
 });
 
